ad83547753e2bae33a1610b4ee3d2c9fa9e5b539
Sajjad106 <sajjadmashfi12@gmail.com>
2023-09-11 11:56:37 +0600

    spelling corrected

--- my_test/my_test/models.py
+++ my_test/my_test/models.py
@@ -1,15 +1,16 @@
 from django.db import models
+from django import forms
 class StudentEntry(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    grade = models.IntegerField(max_length=12)
+    grade = models.PositiveIntegerField()
     mothers_name = models.CharField(max_length=100)
     fathers_name = models.CharField(max_length=100)
-    gurdians_name = models.CharField(max_length=100)
+    guardians_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(default='1988-01-01')
     contact_no = models.CharField(max_length=20)
-    peresent_adress = models.TextField(max_length=100)
-    permanent_adress = models.TextField(max_length=100 )
+    present_address = models.TextField(max_length=100)
+    permanent_address = models.TextField(max_length=100)
     
     
     
--- my_test/my_test/views.py
+++ my_test/my_test/views.py
@@ -9,24 +9,28 @@ def insert(request):
     grade = request.POST.get('grade')
     mothers_name = request.POST.get('mothers_name')
     fathers_name = request.POST.get('fathers_name')
-    gurdian_name = request.POST.get('gurdian_name')
+    guardians_name = request.POST.get('guardians_name')
     date_of_birth = request.POST.get('date_of_birth')
     contact_no = request.POST.get('contact_no')
-    persent_adress = request.POST.get('persent_adress')
-    permanent_adress = request.POST.get('permanent_adress')
+    present_address = request.POST.get('present_address')
+    permanent_address = request.POST.get('permanent_address')
     
     st_obj = StudentEntry()
+    
     st_obj.name = student_name
     st_obj.grade = grade
     st_obj.mothers_name = mothers_name
     st_obj.fathers_name = fathers_name
-    st_obj.gurdians_name = gurdian_name
+    st_obj.guardians_name = guardians_name
     st_obj.date_of_birth = date_of_birth
     st_obj.contact_no = contact_no
-    st_obj.peresent_adress = persent_adress
-    st_obj.permanent_adress = permanent_adress
+    st_obj.present_address = present_address
+    st_obj.permanent_address = permanent_address
     st_obj.save()
+    st = [student_name, st_obj, st_obj.mothers_name, st_obj.fathers_name, st_obj.guardians_name, st_obj.date_of_birth, st_obj.contact_no, st_obj.present_address, st_obj.permanent_address]
+    
     messages.success(request, "Student Information Inserted Successfully")
+    
     return redirect('studentadmin')
 
   
