3d5aedb86e9d114bfe7bc3c41b3109805f3caa4f
Sajjad106 <sajjadmashfi12@gmail.com>
2023-09-11 09:21:46 +0600

    urls error fixed and success message added

--- my_test/my_test/models.py
+++ my_test/my_test/models.py
@@ -2,14 +2,14 @@ from django.db import models
 class StudentEntry(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    grade = models.IntegerField(max_length=12, null=True)
-    mothers_name = models.CharField(max_length=100, default='Unknown')
-    fathers_name = models.CharField(max_length=100, default='Unknown')
-    gurdians_name = models.CharField(max_length=100, default='Unknown')
-    date_of_birth = models.DateField(default='1988-01-01', blank=True)
-    contact_no = models.CharField(max_length=20, default='N/A')
-    peresent_adress = models.TextField(max_length=100, default='N/A', null=False)
-    permanent_adress = models.TextField(max_length=100, default='N/A', null=False)
+    grade = models.IntegerField(max_length=12)
+    mothers_name = models.CharField(max_length=100)
+    fathers_name = models.CharField(max_length=100)
+    gurdians_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(default='1988-01-01')
+    contact_no = models.CharField(max_length=20)
+    peresent_adress = models.TextField(max_length=100)
+    permanent_adress = models.TextField(max_length=100 )
     
     
     
--- my_test/my_test/views.py
+++ my_test/my_test/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, HttpResponse, redirect
 from .models import StudentEntry 
+from django.contrib import messages as messages
 
 def student(request):
     return render(request, 'admin/student.html') 
 def insert(request):
     student_name = request.POST.get('student_name')
-    grades = request.POST.get('grades')
+    grade = request.POST.get('grade')
     mothers_name = request.POST.get('mothers_name')
     fathers_name = request.POST.get('fathers_name')
     gurdian_name = request.POST.get('gurdian_name')
@@ -16,7 +17,7 @@ def insert(request):
     
     st_obj = StudentEntry()
     st_obj.name = student_name
-    st_obj.grade = grades
+    st_obj.grade = grade
     st_obj.mothers_name = mothers_name
     st_obj.fathers_name = fathers_name
     st_obj.gurdians_name = gurdian_name
@@ -25,6 +26,7 @@ def insert(request):
     st_obj.peresent_adress = persent_adress
     st_obj.permanent_adress = permanent_adress
     st_obj.save()
+    messages.success(request, "Student Information Inserted Successfully")
     return redirect('studentadmin')
 
   
